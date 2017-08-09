4676ec1b9d5daac8c1c02beddd40473a6346cac6
Jonas Stupnik <jonas.stupnik@gmail.com>
2017-08-09 02:19:55 -0400

    fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,3 +64,12 @@
     </div>
 </div>
 @endsection
+
+@section('scripts')
+    <script src="DataTables/jQuery-2.2.4/jquery-2.2.4.min.js"></script>
+    <script src="DataTables/Bootstrap-3.3.7/js/bootstrap.min.js"></script>
+@endsection
+
+@section(('stylesheets'))
+    <link href="/Datatables/Bootstrap-3.3.7/css/bootstrap.min.css" rel='stylesheet' type='text/css'>
+@endsection
